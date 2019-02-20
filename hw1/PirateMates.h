0036c411c8b482e84126fbf23c605c855e62c44a
Alyssa Rose <32802037+alyssa-rose@users.noreply.github.com>
2019-02-20 17:58:38 -0500

    Add files via upload

---
+++ hw1/PirateMates.h
@@ -0,0 +1,16 @@
+/*
+* PirateMates.h
+* Alyssa Rose
+* 24-Jan-2019
+* COMP15
+* Spring 2019
+* Creates PirateMates struct
+*/
+
+// Creates PirateMates struct that
+// contains a Pirate and ArrayList object
+struct PirateMates {
+	Pirate pirate;
+	ArrayList mates;
+
+};
